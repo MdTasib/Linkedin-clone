58754c83e72bbc0b58c7e57944f90f172dddaeb8
MdTasib <ohidulalam442@gmail.com>
2021-07-14 11:07:51 +0600

    building the input options

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create';
+import InputOption from '../InputOption/InputOption';
+import PanoramaIcon from '@material-ui/icons/Panorama';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import VerticalSplitIcon from '@material-ui/icons/VerticalSplit';
 
 function Feed() {
     return (
@@ -9,9 +14,15 @@ function Feed() {
                 <div className="feed__input">
                     <CreateIcon />
                     <form>
-                        <input type="text" />
+                        <input type="text" placeholder='Start a post' />
                         <button type="submit">Send</button>
                     </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={PanoramaIcon} title='Photo' color='#70b5f9' />
+                    <InputOption Icon={YouTubeIcon} title='Video' color='#7fc15e' />
+                    <InputOption Icon={EventNoteIcon} title='Event' color='#e7a33e' />
+                    <InputOption Icon={VerticalSplitIcon} title='Write article' color='#fc9295' />
                 </div>
             </div>
         </div>
